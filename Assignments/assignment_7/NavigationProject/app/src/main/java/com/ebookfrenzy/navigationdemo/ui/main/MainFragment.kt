803fa08d663a67034a6660f8a579e1a29951f47e
ConnorLMurphy <colimurphy@wccnet.edu>
2022-11-04 23:27:55 -0400

    updates

---
+++ Assignments/assignment_7/NavigationProject/app/src/main/java/com/ebookfrenzy/navigationdemo/ui/main/MainFragment.kt
@@ -0,0 +1,67 @@
+package com.ebookfrenzy.navigationdemo.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ebookfrenzy.navigationdemo.R
+import androidx.navigation.Navigation
+
+import com.ebookfrenzy.navigationdemo.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.button1.setOnClickListener {
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+
+            action.setMessage(binding.button1.text.toString())
+            action.setImage(R.drawable.android_image_1)
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+        binding.button2.setOnClickListener {
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+
+            action.setMessage(binding.button2.text.toString())
+            action.setImage(R.drawable.android_image_2)
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+        binding.button3.setOnClickListener {
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+
+            action.setMessage(binding.button3.text.toString())
+            action.setImage(R.drawable.android_image_3)
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+    }
+
+}
